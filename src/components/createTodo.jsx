e53ff97fd5751ac8f2f803e82dd67d4db22136c1
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-11-02 00:04:52 +0530

    Update createTodo.jsx

--- src/components/createTodo.jsx
+++ src/components/createTodo.jsx
@@ -22,7 +22,7 @@ export function CreateTodo({ editData }) {
         title: "",
         content: "",
         isCommplete: false
-    })
+    });
     useEffect(() => {
         if (editData) {
             setNodeCreate({
